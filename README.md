1b39097edce345fcb539a88726788d02598d16f6
Ivan Nikolaenko <nikolaenkostudio@gmail.com>
2023-03-10 20:56:14 +0300

    appveyor connected

---
+++ README.md
@@ -0,0 +1,3 @@
+# Task Classes Methods
+
+[![Build status](https://ci.appveyor.com/api/projects/status/mupb4g0pylcojp60?svg=true)](https://ci.appveyor.com/project/Nikoivan/classes-methods)
--- test/test.js
+++ test/test.js
@@ -1,18 +1,18 @@
-import Character from '../src/js/character';
-import Bowman from '../src/js/bowman';
-import Swordsman from '../src/js/swordsman';
-import Magician from '../src/js/magician';
-import Daemon from '../src/js/daemon';
-import Undead from '../src/js/undead';
-import Zombie from '../src/js/zombie';
+import Character from "../src/js/character";
+import Bowman from "../src/js/bowman";
+import Swordsman from "../src/js/swordsman";
+import Magician from "../src/js/magician";
+import Daemon from "../src/js/daemon";
+import Undead from "../src/js/undead";
+import Zombie from "../src/js/zombie";
 
 const playersList = [
   [
     Bowman,
-    'Ivan',
+    "Ivan",
     {
-      name: 'Ivan',
-      type: 'Bowman',
+      name: "Ivan",
+      type: "Bowman",
       health: 100,
       level: 1,
       attack: 25,
@@ -21,10 +21,10 @@ const playersList = [
   ],
   [
     Swordsman,
-    'Yana',
+    "Yana",
     {
-      name: 'Yana',
-      type: 'Swordsman',
+      name: "Yana",
+      type: "Swordsman",
       health: 100,
       level: 1,
       attack: 40,
@@ -33,10 +33,10 @@ const playersList = [
   ],
   [
     Magician,
-    'Alexander',
+    "Alexander",
     {
-      name: 'Alexander',
-      type: 'Magician',
+      name: "Alexander",
+      type: "Magician",
       health: 100,
       level: 1,
       attack: 10,
@@ -45,10 +45,10 @@ const playersList = [
   ],
   [
     Daemon,
-    'Tanya',
+    "Tanya",
     {
-      name: 'Tanya',
-      type: 'Daemon',
+      name: "Tanya",
+      type: "Daemon",
       health: 100,
       level: 1,
       attack: 10,
@@ -57,10 +57,10 @@ const playersList = [
   ],
   [
     Undead,
-    'Valya',
+    "Valya",
     {
-      name: 'Valya',
-      type: 'Undead',
+      name: "Valya",
+      type: "Undead",
       health: 100,
       level: 1,
       attack: 25,
@@ -69,10 +69,10 @@ const playersList = [
   ],
   [
     Zombie,
-    'Oleg',
+    "Oleg",
     {
-      name: 'Oleg',
-      type: 'Zombie',
+      name: "Oleg",
+      type: "Zombie",
       health: 100,
       level: 1,
       attack: 40,
@@ -84,18 +84,18 @@ const playersList = [
 const testHandler = test.each(playersList);
 
 testHandler(
-  'testing player %q created by %w',
+  "testing player %q created by %w",
   (Type, createdObject, expectedObject) => {
     const result = new Type(createdObject);
     expect(result).toEqual(expectedObject);
-  },
+  }
 );
 
-test('test for class Character', () => {
-  const result = new Character('Matvey', 'Daemon');
+test("test for class Character", () => {
+  const result = new Character("Matvey", "Daemon");
   expect(result).toEqual({
-    name: 'Matvey',
-    type: 'Daemon',
+    name: "Matvey",
+    type: "Daemon",
     health: 100,
     level: 1,
     attack: 10,
@@ -104,42 +104,42 @@ test('test for class Character', () => {
 });
 
 const characterErrorList = [
-  ["Error - name isn't string", 1, 'Daemon', 'Ошибка в параметре name'],
-  ['Error - name is too short', 'a', 'Daemon', 'Ошибка в параметре name'],
+  ["Error - name isn't string", 1, "Daemon", "Ошибка в параметре name"],
+  ["Error - name is too short", "a", "Daemon", "Ошибка в параметре name"],
   [
-    'Error - name is too long',
-    'AAAAAAAAAbbbbbddddduuuuullllaaaaeeevvvvv',
-    'Daemon',
-    'Ошибка в параметре name',
+    "Error - name is too long",
+    "AAAAAAAAAbbbbbddddduuuuullllaaaaeeevvvvv",
+    "Daemon",
+    "Ошибка в параметре name",
   ],
   [
-    'Error - incorrect type of player',
-    'Matvey',
-    'Demon',
-    'Ошибка в типе игрока',
+    "Error - incorrect type of player",
+    "Matvey",
+    "Demon",
+    "Ошибка в типе игрока",
   ],
 ];
 
 const testCharacterErrors = test.each(characterErrorList);
 
 testCharacterErrors(
-  'test Errors of class Character by name is %q type is %w',
+  "test Errors of class Character by name is %q type is %w",
   (error, name, type, expected) => {
     expect(() => {
       const result = new Character(name, type);
       console.log(result);
     }).toThrowError(expected);
-  },
+  }
 );
 
-test('test for method levelUp of class Character', () => {
-  const result = new Character('Matvey', 'Daemon');
+test("test for method levelUp of class Character", () => {
+  const result = new Character("Matvey", "Daemon");
 
   result.levelUp();
 
   expect(result).toEqual({
-    name: 'Matvey',
-    type: 'Daemon',
+    name: "Matvey",
+    type: "Daemon",
     health: 100,
     level: 2,
     attack: 12,
@@ -147,21 +147,21 @@ test('test for method levelUp of class Character', () => {
   });
 });
 
-test('test of Error by the method levelUp in the class Character', () => {
+test("test of Error by the method levelUp in the class Character", () => {
   expect(() => {
-    const result = new Character('Matvey', 'Daemon');
+    const result = new Character("Matvey", "Daemon");
     result.health = 0;
     result.levelUp();
-  }).toThrowError('Нельзя повысить левел умершего');
+  }).toThrowError("Нельзя повысить левел умершего");
 });
 
 const damageList = [
   [
-    'Damage is 50 points',
+    "Damage is 50 points",
     50,
     {
-      name: 'Matvey',
-      type: 'Daemon',
+      name: "Matvey",
+      type: "Daemon",
       health: 55,
       level: 1,
       attack: 10,
@@ -169,11 +169,11 @@ const damageList = [
     },
   ],
   [
-    'Damage is 500 points',
+    "Damage is 500 points",
     500,
     {
-      name: 'Matvey',
-      type: 'Daemon',
+      name: "Matvey",
+      type: "Daemon",
       health: 0,
       level: 1,
       attack: 10,
@@ -185,11 +185,11 @@ const damageList = [
 const damageTestHandler = test.each(damageList);
 
 damageTestHandler(
-  'Test %q in class Character',
+  "Test %q in class Character",
   (checkName, points, expected) => {
-    const result = new Character('Matvey', 'Daemon');
+    const result = new Character("Matvey", "Daemon");
     console.log(checkName);
     result.damage(points);
     expect(result).toEqual(expected);
-  },
+  }
 );
